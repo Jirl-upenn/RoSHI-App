c318112a6cd1b031719ae735214b3248773c4999
Luyang Hu <hulu@umich.edu>
2026-01-07 19:49:47 -0500

    Initial Commit

---
+++ ROSHI/ROSHIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ROSHIApp.swift
+//  ROSHI
+//
+//  Created by Luyang Hu on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ROSHIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
